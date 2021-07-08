21f92a2fd3eeba5bdf20452b4200383e0f5f74fe
Uriy Jykov <ldomofon-sericel@rambler.ru>
2021-07-08 22:02:05 +0300

    first commit

--- css-sandbox/position-css/script.js
+++ css-sandbox/position-css/script.js
@@ -1,8 +1,10 @@
 let hello = 'Hello world!'
 var hello2 = 'Hello world 2!'
+const hello3 = 'Hello world 3!'
 
 //while (true) {
 //alert('Hello world!')
 //} 
 console.log(hello)
 console.log(hello2)
+console.log(hello3)
