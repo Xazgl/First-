a003ee34874749cc2cc82a84992eb3f936bf61f0
Uriy Jykov <ldomofon-sericel@rambler.ru>
2021-07-19 20:39:46 +0300

    java1

--- css-sandbox/position-css/script.js
+++ css-sandbox/position-css/script.js
@@ -11,13 +11,29 @@ console.log(hello2)
 console.log(hello3)
 
 //типы данных
+
 let stringVar='Some string'//строка
+console.log (typeof stringVar) //выводит тип данных (выше) в консоль 
+
 let numberVar= 2134.3435//число
+console.log (typeof numberVar)
+
 let booleanVar= true //false //
+console.log (typeof booleanVar)//выводит тип данных (выше) в консоль 
+
 let underfinedVar //=undefined  //тип данных не объявлен можно много что в нее положить 
+console.log (typeof underfinedVar )//выводит тип данных (выше) в консоль 
+
 let nillVar= null // 
+console.log (typeof nillVar)//выводит тип данных (выше) в консоль 
 
+let objectVar= { 
+key1:'Value 1',
+key2:'Value 2'
+} // создали объект 
 
+console.log (typeof objectVar) //выводит тип данных (выше) в консоль 
+console.log (objectVar.key1) //выводит ключ 1
 
 let header = document.getElementById('.header')//считали эелемент по id и присвоили переменную,справа на лево//
 header.textContent= "Привет" //меняем название, либо вставляем hello3 //
