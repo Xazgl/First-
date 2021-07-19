1c93b3ad1cde6fa8ada27c982823527f8f8f2711
Uriy Jykov <ldomofon-sericel@rambler.ru>
2021-07-19 20:23:14 +0300

    java

--- css-sandbox/position-css/script.js
+++ css-sandbox/position-css/script.js
@@ -5,6 +5,19 @@ const hello3 = 'Hello world 3!'
 //while (true) {
 //alert('Hello world!')
 //} 
+
 console.log(hello)
 console.log(hello2)
 console.log(hello3)
+
+//типы данных
+let stringVar='Some string'//строка
+let numberVar= 2134.3435//число
+let booleanVar= true //false //
+let underfinedVar //=undefined  //тип данных не объявлен можно много что в нее положить 
+let nillVar= null // 
+
+
+
+let header = document.getElementById('.header')//считали эелемент по id и присвоили переменную,справа на лево//
+header.textContent= "Привет" //меняем название, либо вставляем hello3 //
